ff2509a760261ad49a7594c77716961d88aa737f
shivaacodes <shivasajay007@gmail.com>
2024-10-13 18:22:59 +0530

    add prisma schema

---
+++ prisma/migrations/20241013125217_init/migration.sql
@@ -0,0 +1,41 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `emailVerified` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `image` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `Account` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `passwordHash` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `role` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('WORKER', 'RESIDENT');
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "emailVerified",
+DROP COLUMN "image",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "passwordHash" TEXT NOT NULL,
+ADD COLUMN     "role" "UserRole" NOT NULL,
+ALTER COLUMN "name" SET NOT NULL,
+ALTER COLUMN "email" SET NOT NULL;
+
+-- DropTable
+DROP TABLE "Account";
+
+-- CreateTable
+CREATE TABLE "Report" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "details" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Report_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Report" ADD CONSTRAINT "Report_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
